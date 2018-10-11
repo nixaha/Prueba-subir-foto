3905565dec02aa3316a48bf9e337c88867e3e5e7
nixaha <nixaha_saucedo@hotmail.com>
2018-10-11 15:56:38 -0500

    subir foto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireStorage } from 'angularfire2/storage';
+import { AngularFireModule } from '../../node_modules/@angular/fire';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBXGUAREAYEc7Z833QjaeOCd7HxACIY99M",
+    authDomain: "prueba-3d2d5.firebaseapp.com",
+    databaseURL: "https://prueba-3d2d5.firebaseio.com",
+    projectId: "prueba-3d2d5",
+    storageBucket: "",
+    messagingSenderId: "29557385502"
+    }),
+    AngularFireStorage 
   ],
   providers: [],
   bootstrap: [AppComponent]
